3fdeba230b153832b1615a4c263afd09be4dcd0a
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 18:53:23 +0200

    TDA Cola

---
+++ src/tdas/Cola.java
@@ -0,0 +1,28 @@
+package tdas;
+
+import java.util.ArrayDeque;
+
+/**
+ * TDA Cola genérica.
+ * Esta clase no es necesaria pues podríamos usar los métodos por defecto de esta clase
+ * posiblemente desaparezca en refactorización.
+ * @param <E> elemento de cola genérica
+ */
+public class Cola<E> extends ArrayDeque<E> {
+
+    /**
+     * Encola un elemento al final
+     * @param item elemento a encolar
+     */
+    public void encolar(E item) {
+        this.add(item);
+    }
+
+    /**
+     * Elimina el elemento al comienzo
+     * @return comienzo
+     */
+    public E desencolar() {
+        return this.removeFirst();
+    }
+}
