574e28a786aab7ca3c7df483070069bf7247b51b
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 18:41:00 +0200

    Sala

---
+++ src/salas/Sala.java
@@ -0,0 +1,33 @@
+package salas;
+
+import peligros.Peligro;
+
+/**
+ * Clase sala. La definimos como un POJO para tener sus métodos típicos usando data
+ * Las propiedades son finales, porque una vez creada la sala no cambia
+ */
+public final class Sala {
+    private final int numero;
+    private final Peligro peligro;
+
+    public Sala(int numero, Peligro peligro) {
+        this.numero = numero;
+        this.peligro = peligro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Peligro getPeligro() {
+        return peligro;
+    }
+
+    @Override
+    public String toString() {
+        return "Sala{" +
+                "numero=" + numero +
+                ", peligro=" + peligro +
+                '}';
+    }
+}
