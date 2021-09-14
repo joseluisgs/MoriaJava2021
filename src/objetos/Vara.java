39696452a5ec40c0774947ab136a081dc26e835d
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 16:55:57 +0200

    Vara

---
+++ src/objetos/Vara.java
@@ -0,0 +1,34 @@
+package objetos;
+
+/**
+ * Clase del objeto Vara
+ */
+public final class Vara extends Objeto {
+    private int energia;
+
+    /**
+     * Constructor de Vara
+     * @param energia Energía de la vara
+     */
+    public Vara(int energia) {
+        super("Vara");
+        this.energia = energia;
+    }
+
+    /**
+     * Obtiene la energía de la vara
+     * @return energía
+     */
+    public int getEnergia() {
+        return this.energia;
+    }
+
+    /**
+     * Establece el valor de la energía de l avara
+     * @param energia valor de energía
+     */
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+}
