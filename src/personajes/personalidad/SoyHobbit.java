1283d6dfc00f26a8488b6cae660b5f56a1fb0694
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 17:16:54 +0200

    SoyHobbit

---
+++ src/personajes/personalidad/SoyHobbit.java
@@ -0,0 +1,16 @@
+package personajes.personalidad;
+
+/**
+ * Interfaz de Personalidad de Hobbit
+ */
+public interface SoyHobbit {
+    /**
+     * Acción de ponerse el Anillo
+     */
+    void ponerseAnillo();
+
+    /**
+     * Acción de quitarse el anillo
+     */
+    void quitarseAnillo();
+}
