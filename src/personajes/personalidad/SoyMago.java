cd30eee86ea94837953e547326a782425459c030
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 17:19:51 +0200

    SoyMago

---
+++ src/personajes/personalidad/SoyMago.java
@@ -0,0 +1,18 @@
+package personajes.personalidad;
+
+/**
+ * Interfaz de Personalidad de Mago
+ */
+public interface SoyMago {
+    /**
+     * Acción de recargar la vara con energía
+     * @param energia energía de la vara
+     */
+    void recargarVara(int energia);
+
+    /**
+     * obtiene el poder de la vara
+     * @return devuleve el poder de la vara
+     */
+    int poderVara();
+}
