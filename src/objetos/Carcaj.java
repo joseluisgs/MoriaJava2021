eb300d557af2d9823211df5fa9f7c7bcdd837ab7
joseuisgs <joseluisgonsan@gmail.com>
2021-09-14 16:50:37 +0200

    Carcaj

---
+++ src/objetos/Carcaj.java
@@ -0,0 +1,33 @@
+package objetos;
+
+/**
+ * Clase de Carcaj
+ */
+public final class Carcaj extends Objeto {
+    private int cantidad;
+
+    /**
+     * Constructor de carcaj
+     * @param cantidad Número de Flechas iniciales
+     */
+    public Carcaj(int cantidad) {
+        super("Carcaj");
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * Devuelve la cantidad de flechas que tiene el carcaj
+     * @return cantidad de flechas
+     */
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    /**
+     * Establece la cantidad de flechas del carcaj
+     * @param cantidad cantidad de flechas
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
