0806d927f4f028524d8632e0ec11400010b04589
joseuisgs <joseluisgonsan@gmail.com>
2021-09-15 17:48:48 +0200

    collection, extensión de interfaz

--- src/moria/tdas/ICola.java
+++ src/moria/tdas/ICola.java
@@ -1,9 +1,11 @@
 package moria.tdas;
 
+import java.util.Collection;
+
 /**
  * Contrato que toda Cola debe cumplir
  */
-public interface ICola<E> {
+public interface ICola<E> extends Collection<E> {
     /**
      * Encola un item
      * @param item item a encolar
